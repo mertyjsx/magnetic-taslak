6baa901843db1fd07ce11b081e8dd3a7703ace89
mertyjsx <orkunmertgs41@gmail.com>
2021-01-07 14:26:54 +0300

    s

--- pages/_document.js
+++ pages/_document.js
@@ -24,11 +24,14 @@ class MyDocument extends Document {
                   `,
                 }}
               />
+              <title>Magnetic Sand Necklace</title>
+              <meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
+              <meta name="og:title" property="og:title" content="stress relief necklaces jewelry"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta name="description" content="Necklace , pendant , jewelry made by sea sand that has magnetic effect from Black Sea region"/>
-<meta name="keywords" content="magnetic,sand,stone,seasand,gift,breastmilk jewelry,magnetic sand necklace,pendant,stress,age,health,anxiety,necklace,etsy"/>
+<meta name="keywords" content="magnetic,sand,stone,seasand,gift,breastmilk jewelry,magnetic sand necklace,pendant,stress,age,health,anxiety,necklace,etsy , stress relief , gemstone"/>
 <meta name="robots" content="index, follow"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
+
 <meta name="language" content="English"/>
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
