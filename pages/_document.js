763c1f6687b68433185593035e4f0b4063984c57
mertyjsx <orkunmertgs41@gmail.com>
2021-01-08 15:26:14 +0300

    s

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,57 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-          <Head />
+           
+    <Head >
+    
+  
+    
+    
+    
+    <title>Magnetic Sand Necklace for Stress Relief Anxiety </title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+   
+   
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    ></meta>
+    <meta
+      name="description"
+      content="Magnetic black sand beach hawaii ,island.Crystal store near me,Stress relief,gemstone list,how does kinetic sand work ,magnetite uses,handmade on amazon
+      "
+    />
+    <meta
+      name="keywords"
+      content="magnetic,sand,stone,seasand,gift,breastmilk jewelry,magnetic sand necklace,pendant,stress,age,health,anxiety,necklace,etsy , stress relief , gemstone"
+    />
+    
+    <meta name="robots" content="index, follow" />
+
+    <meta name="thumbnail" content="/thumbnail.jpg" />
+   
+   
+    <meta property="og:type" content="website" />
+   
+   
+   
+    <meta  name="og:title" property="og:title" content="Magnetic sand necklace , stress relief jewelry"></meta>
+    <meta name="og:url" property="og:type" content="product"/>
+    <meta property="og:image" content="https://magneticsand.net/thumbnail.jpg" />
+
+   
+    <link
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      rel="stylesheet"
+    ></link>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    ></link>
+  
+         
+  
+      </Head>,
         <body>
           <Main />
           <NextScript />
