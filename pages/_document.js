4a29a50092a8a9fdcb0622332ce9583bce91565a
mertyjsx <orkunmertgs41@gmail.com>
2021-01-09 12:20:33 +0300

    s

--- pages/_document.js
+++ pages/_document.js
@@ -10,16 +10,19 @@ class MyDocument extends Document {
     <Head >
     
   
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179714695-1">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-179714695-1');
-</script>
     
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-179714695-1"></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'UA-179714695-1');
+                  `,
+                }}
+              />
     
     
     <title>Magnetic Sand Necklace for Stress Relief Anxiety </title>
