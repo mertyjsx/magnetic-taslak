393cdf80704195305ff7cafcafcd5b0f902ec31d
mertyjsx <orkunmertgs41@gmail.com>
2021-02-22 01:33:21 +0300

    s

--- pages/_document.js
+++ pages/_document.js
@@ -56,14 +56,7 @@ class MyDocument extends Document {
 
     ></link>
    
-    <link
-    
-      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
-       
-      rel="stylesheet"
-      
-    ></link>
-  
+   
          
   
       </Head>,
